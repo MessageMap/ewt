c2a5c00e32e337e316adcdc8b4e355eff6d09ebb
evgeny <eugeneim@gmail.com>
2015-08-08 18:09:51 +0300

    bits and pieces

--- src/ewt.erl
+++ src/ewt.erl
@@ -15,47 +15,55 @@ token(Expiration, Claims_, Key, Alg) ->
 	B64Header = base64url:encode(term_to_binary(Header)),
 	B64Claims = base64url:encode(term_to_binary(Claims)),
 
-	HeaderClaims = <<B64Header/binary, ".", B64Claims/binary>>,
+	Payload = payload(B64Header, B64Claims),
 
-	B64Signature = sign(?TYPE, alg(Alg), B64Header, B64Claims, Key),
+	B64Signature = sign(?TYPE, alg(Alg), Payload, Key),
 
-	<<HeaderClaims/binary, ".", B64Signature/binary>>.
+	<<Payload/binary, ".", B64Signature/binary>>.
 
 
 claims_dated(Token, Key) ->
-	case catch parse(Token, Key, dated) of
-		expired -> expired;
-		{ok, Claim} -> {ok, Claim};
-		_ -> bad
-	end.
+	claims(Token, Key, dated).
 
 
 claims(Token, Key) ->
-	case catch parse(Token, Key) of
+	claims(Token, Key, standard).
+
+claims(Token, Key, Mode) ->
+	case catch parse(Token, Key, Mode) of
 		expired -> expired;
 		{ok, Claim} -> {ok, Claim};
 		_ -> bad
 	end.
 
+
 parse(Token, Key, dated) ->
 	[_Date, Rest] = binary:split(Token, <<".">>),
-	parse(Rest, Key).
+	parse(Rest, Key, standard);
 
-parse(Token, Key) ->
+parse(Token, Key, standard) ->
 	[B64Header, B64Claims, B64Signature] = binary:split(Token, <<".">>, [global]),
 	#{typ := Type, alg := Alg} = binary_to_term(base64url:decode(B64Header), [safe]),
 	true = B64Signature == sign(Type, Alg, B64Header, B64Claims, Key),
-	#{exp := Exp} = Claims = binary_to_term(base64url:decode(B64Claims), [safe]),
+	Claims = binary_to_term(base64url:decode(B64Claims), [safe]),
+	check_expired(Claims).
+
+
+check_expired(#{exp := Exp} = Claims) ->
 	Now = calendar:datetime_to_gregorian_seconds(calendar:universal_time()),
-	if
-		Now > Exp -> expired;
-		true -> {ok, Claims}
-	end.
+	check_expired(Claims, Now, Exp).
 
-sign(?TYPE, Alg, B64Header, B64Claims, Key) ->
-	base64url:encode(crypto:hmac(Alg, Key, <<B64Header/binary, ".", B64Claims/binary>>)).
+check_expired(_Claims, Now, Exp) when Now > Exp -> expired;
+check_expired(Claims, _Now, _Exp) -> {ok, Claims}.
 
+sign(?TYPE, Alg, Header, Claims, Key) ->
+	Payload = payload(Header, Claims),
+	sign(?TYPE, Alg, Payload, Key).
+sign(?TYPE, Alg, Payload, Key) ->
+	base64url:encode(crypto:hmac(Alg, Key, Payload)).
 
+payload(Header, Claims) ->
+	<<Header/binary, ".", Claims/binary>>.
 
 alg(auto) -> ?DEFAULT_ALG;
 alg(Alg) -> Alg.
